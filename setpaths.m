500492ed027c3bf536149e38db4a2a83ce64adf9
Wilson <qi.wu@cern.ch>
2016-04-06 01:37:47 +0800

    update setpaths

--- setpaths.m
+++ setpaths.m
@@ -7,12 +7,14 @@
 % More info: http://pillowlab.princeton.edu/code_GLM.html
 % Github page: https://github.com/pillowlab/GLMspiketools
 
-
 basedir = pwd;  % The directory where this script lives
+GLMspiketools_path = ...
+   {[basedir '/glmtools_fitting/'],...
+    [basedir '/glmtools_misc/'],...
+    [basedir '/nlfuns/']};
 
 % Add a bunch sub-directories (with absoluate path names)
-addpath([basedir '/glmtools_fitting/']);
-addpath([basedir '/glmtools_misc/']);
-addpath([basedir '/glmtools_mex/']);
-addpath([basedir '/nlfuns/']);
-
+for m_i = 1:length(GLMspiketools_path)
+   addpath(GLMspiketools_path{m_i});
+end
+clear m_i;
